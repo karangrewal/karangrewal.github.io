1b1aced89d3811b21be8dcb9b5aa308ff58648e1
karangrewal <karan_gre_wal@hotmail.com>
2018-04-13 20:18:02 -0400

    new template

---
+++ ip.php
@@ -0,0 +1,1 @@
+$("#copyFooter").append(" - IP: <a style=\"text-decoration:none\" href=\"javascript:void(0)\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Used for statistical purposes only.\">99.229.219.201</a>");
---
+++ js/footer.js
@@ -0,0 +1,4 @@
+function fillFooter()
+{
+    $('#copyFooter').prepend('&copy; 2017 Jacopo De Simoi - Powered by <a href="http://twitter.github.com/bootstrap/"> Twitter bootstrap</a>');
+}
