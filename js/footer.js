4b92a7c77ef44f0b4d8376b2003716f2b0bf49c8
karangrewal <karan_gre_wal@hotmail.com>
2018-04-13 20:39:30 -0400

    modifying footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
 function fillFooter()
 {
-    $('#copyFooter').prepend('&copy; 2017 Jacopo De Simoi - Powered by <a href="http://twitter.github.com/bootstrap/"> Twitter bootstrap</a>');
+    $('#copyFooter').prepend('&copy; 2018 Karan Grewal - Powered by <a href="http://twitter.github.com/bootstrap/"> Twitter bootstrap</a>');
 }
